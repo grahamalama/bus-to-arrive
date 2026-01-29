33ec67339f133cb06d1ce9d63034b4580bce8f8d
Graham Beckley <graham.beckley@gmail.com>
2026-01-28 19:50:14 -0500

    CLI skeleton

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import requests
+import argparse
 import json
+
+import requests
 from pydantic import BaseModel
 
 
@@ -18,8 +20,32 @@ def get_current_location() -> Coordinates:
         return None
 
 
+def fetch_closest_stop(locaiton: Coordinates):
+    pass
+
+
+def fetch_next_busses(stop):
+    pass
+
+
 def main():
-    print("Hello from bus-to-arrive!")
+    parser = argparse.ArgumentParser(
+        prog="bts",
+        description="View the next busses to arrive on your route",
+    )
+    parser.add_argument("route", help="bus route you're traveling on")
+    parser.add_argument(
+        "-s", "--start", help="Lat/Lon of where you're starting your journey from"
+    )
+    args = parser.parse_args()
+
+    if args.start is None:
+        # location = get_current_location()
+        location = get_current_location()
+    location = args.start
+
+    stop = fetch_closest_stop(location)
+    busses = fetch_next_busses(stop)
 
 
 if __name__ == "__main__":
